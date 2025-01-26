78175c6b81a9e8a91c972986ad7d84168be31835
Sharik-oss <mamukabigvava2201@gmail.com>
2025-01-26 22:21:19 +0400

    Added movie fields

--- src/app/add-movie/add-movie.component.ts
+++ src/app/add-movie/add-movie.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import { AngularEditorModule } from '@kolkov/angular-editor';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { NgxEditorModule, Editor } from 'ngx-editor'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-add-movie',
-  imports: [MatFormFieldModule, FormsModule, MatInputModule, AngularEditorModule ],
+  imports: [MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    NgxEditorModule,
+    MatCheckboxModule, 
+    MatButtonModule],
   templateUrl: './add-movie.component.html',
   styleUrl: './add-movie.component.scss'
 })
-export class AddMovieComponent {
+export class AddMovieComponent implements OnInit, OnDestroy {
+
+  editor!: Editor;
+  html = '';
+
+
+  ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  // make sure to destory the editor
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 }
